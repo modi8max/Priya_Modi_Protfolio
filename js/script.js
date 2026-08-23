c75be673f63c8c396698aa5944f504f4046bd313
Priya Modi <modip5214@gmail.com>
2026-08-23 12:58:37 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -17,3 +17,13 @@ document.addEventListener("DOMContentLoaded", function () {
     navbarLinks.classList.remove("active");
   });
 });
+
+function openMenu(){
+    document.getElementById("sidebar").classList.add("active");
+    document.getElementById("overlay").classList.add("active");
+}
+
+function closeMenu(){
+    document.getElementById("sidebar").classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
+}
